387661b806cb92820c5af6760b470afac21fa7d3
villiseck <jvk@abes.fr>
2023-06-26 16:22:05 +0200

    Ajout starter web

---
+++ src/test/java/com/render/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.render.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
